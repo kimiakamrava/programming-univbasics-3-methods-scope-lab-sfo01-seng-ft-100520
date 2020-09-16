9c523519dec28aa05b773d0577b5e921300e9d2c
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-16 18:48:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,4 +18,6 @@ end
 link
 
 def all_phrases
-  puts  "It's-a me, Mario!" .'Thank You Mario! But Our Princess Is In Another Castle!'.
+  puts  "It's-a me, Mario!" .'Thank You Mario! But Our Princess Is In Another Castle!'."It's Dangerous To Go Alone! Take This."
+end
+all_phrases
